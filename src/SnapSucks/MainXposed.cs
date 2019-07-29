9ff603f26d82ad81ad5687b9d77fe8b112f4c179
AeonLucid <aeonlucid@gmail.com>
2019-07-29 15:44:59 +0200

    Doesn't compile.

---
+++ src/SnapSucks/MainXposed.cs
@@ -0,0 +1,14 @@
+﻿using Android.Util;
+using Xposed;
+using Xposed.Callbacks;
+
+namespace SnapSucks
+{
+    public class MainXposed : Java.Lang.Object, IXposedHookLoadPackage
+    {
+        public void HandleLoadPackage(XC_LoadPackage.LoadPackageParam p0)
+        {
+            Log.Info("SnapSucks", "Why tho.");
+        }
+    }
+}
